795834e4c5b78452cbaedcc21da2137d7ff4b313
aytajramazanly <aytajshr@code.edu.az>
2022-02-09 17:04:26 +0400

    homework added

---
+++ FrontToBack/FrontToBack/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using FrontToBack.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HeaderViewComponent(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var bio = await _dbContext.Bios.SingleOrDefaultAsync();
+
+            return View(bio);
+        }
+    }
+}
